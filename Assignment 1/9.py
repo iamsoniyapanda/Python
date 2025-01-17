01f19e9262bfae9e90ee25a978a855b964c2eddf
SONIYA PANDA_MCA_1A_47_UEMK_ 2024-2026 <pandasoniya121@gmail.com>
2025-01-18 01:32:16 +0530

    Create 9.py

---
+++ Assignment 1/9.py
@@ -0,0 +1,35 @@
+    *                    
+   * *
+  *   *
+ *     *
+*       *
+ *     *
+  *   *
+   * *
+    * 
+
+n = 5
+for i in range(n):
+    
+    print(" " * (n - i - 1), end="")
+    print("*", end="")
+    if i != 0 and i != n - 1:
+        print(" " * (2 * i - 1), end="")
+    if i != 0 and i != n - 1:
+        print("*", end="")
+    print()
+
+for i in range(n - 2, -1, -1):
+    print(" " * (n - i - 1), end="")
+    
+    print("*", end="")
+
+    if i != 0:
+        print(" " * (2 * i - 1), end="")
+
+    if i != 0:
+        print("*", end="")
+
+    print()
+
+
